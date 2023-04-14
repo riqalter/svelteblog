cebc6450d08e2f34cb112a2261aec72d613d2f84
Mikail Thoriq <mikail.thoriq@gmail.com>
2023-04-14 11:00:33 +0700

    edit site domain

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'riq.my.id',
   title: 'riqblog',
   subtitle: 'riqs experiment',
   lang: 'en-US',
